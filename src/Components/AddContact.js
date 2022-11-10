4917589dee810a46b39460f13e827adb45466f1d
yego <yegokipkemoi254@gmail.com>
2022-11-10 14:00:09 +0300

    implemented submit func

--- src/Components/AddContact.js
+++ src/Components/AddContact.js
@@ -12,7 +12,21 @@ function AddContact() {
     photo:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0g8R5VmgqDqpRCwQfw2Y9V-DM-iXS59R9hQ&usqp=CAU",
   });
-  
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch(`https://sammy-contact-manager.herokuapp.com/contacts`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(contact),
+    })
+      .then((res) => res.json())
+      .then((data) =>{
+            navigate(`/contacts/view/${data.id}`)
+      });
+
   return (
     <div>AddContact</div>
   )
