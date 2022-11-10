83bf2ce447425fd3ad6f15f17592b364c8b35ae5
yego <yegokipkemoi254@gmail.com>
2022-11-10 13:53:27 +0300

    imported components and added props

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 
-function Card() {
+function Card({ contact, handleDelete }) {
   return (
     <div>Card</div>
   )
