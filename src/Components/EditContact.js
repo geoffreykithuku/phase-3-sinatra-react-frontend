7714a69af29d5e7a1fe92a8e884d25670cccfea8
bob <sammy.chesire@student.moringaschool.com>
2022-11-10 13:57:32 +0300

    create an EditContact component

--- src/Components/EditContact.js
+++ src/Components/EditContact.js
@@ -1,67 +1,129 @@
 
-function ContactList(){
+
+function EditContact(){
 
     return (
         <React.Fragment>
-          <section className="contact-search p-3">
-            <div className="container">
-              <div className="grid">
-                <div className="row">
-                  <div className="col">
-                    <p className="h3 fw-bold">
-                      Contact Manager
-                      <Link to={"/contacts/add"} className="btn btn-primary ms-2">
-                        <i className="fa fa-plus-circle me-2"></i>
-                        New
-                      </Link>
-                    </p>
-                    <p className="fst-italic">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                      do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                      Ut enim ad minim veniam
-                    </p>
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-md-6">
-                    <form className="row">
-                      <div className="col">
-                        <div className="mb-2">
-                          <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Search Names"
-                          ></input>
-                        </div>
-                      </div>
-                      <div className="col">
-                        <div className="mb-2">
-                          <input
-                            type="submit"
-                            className="btn btn-outline-dark"
-                            value="Search"
-                          ></input>
-                        </div>
-                      </div>
-                    </form>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
-          <section className="contact-list">
+          <section className="add-contact p-3">
             <div className="container">
               <div className="row">
+                <div className="col">
+                  <p className="h4 text-primary fw-bold">Edit Contact</p>
+                  <p className="fst-italic">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                    enim ad
+                  </p>
+                </div>
+              </div>
+              <div className="row align-items-center">
+                <div className="col-md-4">
+                  <form onSubmit={handleSubmit}>
+                    <div className="mb-2">
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Name"
+                        value={contact.name}
+                        onChange={(e) =>
+                          setContact((contact) => ({
+                            ...contact,
+                            name: e.target.value,
+                          }))
+                        }
+                      ></input>
+                    </div>
+                    <div className="mb-2">
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Photo url"
+                        value={contact.photo}
+                        onChange={(e) =>
+                          setContact((contact) => ({
+                            ...contact,
+                            photo: e.target.value,
+                          }))
+                        }
+                      ></input>
+                    </div>
+                    <div className="mb-2">
+                      <input
+                        type="number"
+                        className="form-control"
+                        placeholder="Phone"
+                        value={contact.mobile}
+                        onChange={(e) =>
+                          setContact((contact) => ({
+                            ...contact,
+                            mobile: e.target.value,
+                          }))
+                        }
+                      ></input>
+                    </div>
+                    <div className="mb-2">
+                      <input
+                        type="email"
+                        className="form-control"
+                        placeholder="Email"
+                        value={contact.email}
+                        onChange={(e) =>
+                          setContact((contact) => ({
+                            ...contact,
+                            email: e.target.value,
+                          }))
+                        }
+                      ></input>
+                    </div>
+                    <div className="mb-2">
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Company"
+                        value={contact.company}
+                        onChange={(e) =>
+                          setContact((contact) => ({
+                            ...contact,
+                            company: e.target.value,
+                          }))
+                        }
+                      ></input>
+                    </div>
+                    <div className="mb-2">
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Title"
+                        value={contact.title}
+                        onChange={(e) =>
+                          setContact((contact) => ({
+                            ...contact,
+                            title: e.target.value,
+                          }))
+                        }
+                      ></input>
+                    </div>
+    
+                    <div className="mb-2">
+                      <input
+                        type="submit"
+                        className="btn btn-primary"
+                        value="Update"
+                      ></input>
+                      <Link to={"/contacts/list"} className="btn btn-dark ms-2">
+                        Cancel
+                      </Link>
+                    </div>
+                  </form>
+                </div>
                 <div className="col-md-6">
-                    {contacts.map(item=><Card key={ item.id} contact={item} handleDelete={handleDelete}/>)}
+                  <img src={contact.photo} alt="user" className="contact-img"></img>
                 </div>
               </div>
             </div>
           </section>
         </React.Fragment>
       );
+}
 
-    }
-
-    export default ContactList;
-
+export default EditContact;
